9401da88f0f53c940dd38e032bedc1c2dcfdd4f6
Christoph Paterok <chptk@AirvonChristoph.fritz.box>
2024-10-26 13:22:15 +0200

    added random messages

--- netlify/functions/check-door.js
+++ netlify/functions/check-door.js
@@ -4,7 +4,8 @@ const { DateTime } = require('luxon');
 exports.handler = async function(event, context) {
   // Your Airtable base and table setup
   const base = new Airtable({ apiKey: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN }).base('apppi8vcU0MF25dgl');
-  const table = base('Doors');
+  const doorsTable = base('Doors');
+  const messagesTable = base('Messages');
 
   // Get the door number from the request
   const doorNumber = event.queryStringParameters.door;
@@ -12,12 +13,18 @@ exports.handler = async function(event, context) {
   // Get the current date in the German time zone
   const currentDate = DateTime.now().setZone('Europe/Berlin');
   const currentDay = currentDate.day;
+  const currentMonth = currentDate.month;
+
+  // Check if it's before December 1st
+  if (currentMonth < 12 || (currentMonth === 12 && currentDay < 1)) {
+    return await getRandomMessage('before_start');
+  }
 
   // Check if the door number is the current day
   if (parseInt(doorNumber) === currentDay) {
     try {
       // Find the row in Airtable where 'Door Number' matches today's date
-      const records = await table.select({
+      const records = await doorsTable.select({
         filterByFormula: `{Door Number} = "${currentDay}"`
       }).firstPage();
 
@@ -42,10 +49,33 @@ exports.handler = async function(event, context) {
       };
     }
   } else {
-    // If the door number is not today's date, return an error
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Dies ist nicht die Tür des Tages.' })
-    };
+    return await getRandomMessage('wrong_day');
   }
 };
+
+async function getRandomMessage(type) {
+  try {
+    const records = await messagesTable.select({
+      filterByFormula: `{Type} = "${type}"`
+    }).all();
+
+    if (records.length > 0) {
+      const randomIndex = Math.floor(Math.random() * records.length);
+      const message = records[randomIndex].get('Message');
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: message })
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Keine Nachricht gefunden.' })
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Fehler beim Abrufen der Nachricht.' })
+    };
+  }
+}
